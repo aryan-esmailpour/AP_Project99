88f3ae32dc3fedad66451950714f99a8236d9d90
Aryan <aryanesmailpour@gmail.com>
2020-06-24 13:26:13 +0430

    simple play and pause buttons added

--- Main.py
+++ Main.py
@@ -1,18 +1,37 @@
-import pygame, sys, time
-from pygame.locals import *
+"""imports"""
+import pygame
+import tkinter as tkr
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello')
+"""song"""
+current_song = "/home/aryan/Desktop/APProject/AP_Project99/songs/Track 2.wav"
 
-pygame.mixer.music.load("/home/aryan/Desktop/APProject/AP_Project99/songs/Track 1.wav")
-pygame.mixer.music.play()
+"""actions"""
+def play_music():
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(current_song)
+    pygame.mixer.music.play()
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+"""window"""
+player_window = tkr.Tk()
+player_window.title("Mamad Music Player ltd.")
+player_window.geometry("260x350")
+
+"""window buttons"""
+play_button = tkr.Button(player_window, width = 5, height = 3, text = "PLAY", command = play_music)
+play_button.pack(fill = "x")
+stop_button = tkr.Button(player_window, width = 5, height = 3, text = "STOP", command = stop_music)
+stop_button.pack(fill = "x")
+
+"""song labels"""
+label1 = tkr.LabelFrame(player_window, text = "Song Name")
+label1.pack(fill = "both", expand = "yes")
+contents1 = tkr.Label(label1, text = "Song.wav")
+contents1.pack()
 
 
-while True: # Main Loop
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+"""run"""
+player_window.mainloop()
