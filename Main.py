6dd44653ee99c67329a798ef7e2879ca924517f7
Aryan <aryanesmailpour@gmail.com>
2020-06-24 15:00:55 +0430

    simple playlist option added

--- Main.py
+++ Main.py
@@ -1,37 +1,56 @@
 """imports"""
+import os
 import pygame
 import tkinter as tkr
 
+"""inits"""
+pygame.init()
+pygame.mixer.init()
+
+"""window"""
+player_window = tkr.Tk()
+player_window.title("Mammad Music Player ltd.")
+player_window.geometry("260x350")
+
+"""playlist"""
+os.chdir("/home/aryan/Desktop/APProject/AP_Project99/songs")
+print(os.getcwd)
+crr_playlist = os.listdir()
+
+"""show playlist"""
+songlist = tkr.Listbox(player_window, highlightcolor = "blue", selectmode = tkr.SINGLE)
+print(crr_playlist)
+for item in crr_playlist:
+    idx = 0
+    songlist.insert(idx, item)
+    idx += 1 
+
 """song"""
-current_song = "/home/aryan/Desktop/APProject/AP_Project99/songs/Track 2.wav"
+#current_song = "Track 2.wav"
 
 """actions"""
 def play_music():
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(current_song)
+    pygame.mixer.music.load(songlist.get(tkr.ACTIVE))
+    var.set(songlist.get(tkr.ACTIVE))
     pygame.mixer.music.play()
 
 def stop_music():
     pygame.mixer.music.stop()
 
-"""window"""
-player_window = tkr.Tk()
-player_window.title("Mamad Music Player ltd.")
-player_window.geometry("260x350")
 
 """window buttons"""
 play_button = tkr.Button(player_window, width = 5, height = 3, text = "PLAY", command = play_music)
-play_button.pack(fill = "x")
 stop_button = tkr.Button(player_window, width = 5, height = 3, text = "STOP", command = stop_music)
-stop_button.pack(fill = "x")
 
 """song labels"""
-label1 = tkr.LabelFrame(player_window, text = "Song Name")
-label1.pack(fill = "both", expand = "yes")
-contents1 = tkr.Label(label1, text = "Song.wav")
-contents1.pack()
+var = tkr.StringVar()
+songtitle = tkr.Label(player_window, textvariable = var)
 
+"""place features"""
+stop_button.pack(fill = "x")
+play_button.pack(fill = "x")
+songtitle.pack()
+songlist.pack(fill = "both", expand = "yes")
 
 """run"""
 player_window.mainloop()
