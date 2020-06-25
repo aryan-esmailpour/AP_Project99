c4532000334da23d63ab8d503dd052a01240596a
reyhanehtt <reyhaneh.tarighatytabesh@gmail.com>
2020-06-25 15:34:35 +0430

    coloring bottoms

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@
 import os
 import pygame
 import tkinter as tkr
-from tkinter import filedialog
 
 """inits"""
 pygame.init()
@@ -11,39 +10,21 @@ pygame.mixer.init()
 """window"""
 player_window = tkr.Tk()
 player_window.title("Mammad Music Player ltd.")
-player_window.geometry("260x450")
+player_window.geometry("260x350")
 
 """playlist"""
-crr_playlist = []
-def playlist_init(tmp_dir):
-    global crr_playlist
-    crr_playlist.clear()
-    os.chdir(tmp_dir)
-    all_list = os.listdir()
-    for item in all_list:
-        if item.endswith(".wav") or item.endswith(".mp3"):
-            crr_playlist.append(item)
-            
-#crr_dir = filedialog.askdirectory()
-#crr_dir = os.getcwd
-#playlist_init(crr_dir)
-#os.chdir(crr_dir)
+os.chdir("C:\Users\Reihaneh\Desktop\AP_Project99\AP_Project99\songs")
 print(os.getcwd)
-#playlist_init(os.getcwd)
-#crr_playlist = os.listdir()
-
+crr_playlist = os.listdir()
 
 """show playlist"""
 songlist = tkr.Listbox(player_window, highlightcolor = "blue", selectmode = tkr.SINGLE)
 print(crr_playlist)
-def update_songlist():
-    global songlist
-    songlist.delete(0,'end')
-    for item in crr_playlist:
-        idx = 0
-        songlist.insert(idx, item)
-        idx += 1 
-update_songlist()
+for item in crr_playlist:
+    idx = 0
+    songlist.insert(idx, item)
+    idx += 1 
+
 """song"""
 #current_song = "Track 2.wav"
 
@@ -66,13 +47,6 @@ def resume_music():
 def update_volume(vlevel):
     #print(vlevel)
     pygame.mixer.music.set_volume(float(vlevel) / 100.0)
-def dir_load():
-    global crr_dir
-    global songlist
-    crr_dir = filedialog.askdirectory()
-    playlist_init(crr_dir)
-    update_songlist()
-
 
 """volume edit"""
 VolumeLevel = tkr.Scale(player_window, from_ = 100, to_ = 0, orient = tkr.VERTICAL, command = update_volume, resolution = 1)
@@ -83,7 +57,6 @@ play_button = tkr.Button(player_window, width = 5, height = 1, text = "PLAY", co
 stop_button = tkr.Button(player_window, width = 5, height = 1, text = "STOP", command = stop_music)
 pause_button = tkr.Button(player_window, width = 5, height = 1, text = "PAUSE", command = pause_music)
 resume_button = tkr.Button(player_window, width = 5, height = 1, text = "GO", command = resume_music)
-browse_button = tkr.Button(player_window, width = 5, height = 1, text = "Browse...", command = dir_load)
 
 
 """song labels"""
